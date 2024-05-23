2dff58e6258d3dc464bd4dffae87baf9bc2ced06
Ritik Nandanwar <nandanwarritik10@gmail.com>
2024-05-23 21:26:40 +0530

    generate random image

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import loading from "./assets/loading.gif";
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  var [isLoading, setIsLoading] = useState(true);
+  var [imageFromTheInternet, setImageFromTheInternet] = useState(
+    "https://picsum.photos/200/300"
+  );
+  useEffect(() => {
+    getImage();
+  }, []);
+  const getImage = async (e) => {
+    // e.preventDefault();
+    setIsLoading(true);
+    const data = await fetch("https://dummyjson.com/products").then((res) =>
+      res.json()
+    );
+    var rand_number = Math.ceil(Math.random() * 29);
+    console.log(data?.products[rand_number]?.images[1]);
+    setImageFromTheInternet(data?.products[rand_number]?.images[1]);
+    setIsLoading(false);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainView}>
+      {isLoading ? (
+        <Image source={loading}></Image>
+      ) : (
+        <View>
+          <Image
+            source={{
+              uri: imageFromTheInternet,
+            }}
+            height={200}
+            width={200}
+          />
+          <Button title="Get Random Image" onPress={getImage} />
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center", //Centered vertically
+    alignItems: "center", //Centered horizontally
   },
 });
